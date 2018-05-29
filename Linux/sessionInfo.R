57aab5ff0290693876da45e079d744c4b30a91b6
Mustafa <myontars@users.noreply.github.com>
2018-05-29 14:43:14 +0300

    Update sessionInfo.R

--- Linux/sessionInfo.R
+++ Linux/sessionInfo.R
@@ -1,5 +1,9 @@
-.libPaths("packrat/packrat_source")
-install.packages("packrat/packrat_source/packrat_0.4.4.tar.gz",lib="packrat/packrat_source/",repos=NULL,type="source")
+path <- Sys.getenv(c("R_LIB_PATH"))
+lib_path <- c(path,"packrat/packrat_source/")
+lib_path <- paste(lib_path,collapse="")
+
+.libPaths(lib_path)
+install.packages("packrat/packrat_source/packrat_0.4.4.tar.gz",lib=lib_path,repos=NULL,type="source")
 
 packrat::on()
 
