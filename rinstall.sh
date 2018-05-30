53f65aa5249efc5f99edb887939591d7cb40d713
Mustafa <myontars@users.noreply.github.com>
2018-05-30 12:19:20 +0300

    Update rinstall.sh
    rlib

--- rinstall.sh
+++ rinstall.sh
@@ -2,4 +2,9 @@ sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD
 sudo add-apt-repository 'deb [arch=amd64,i386] https://cran.rstudio.com/bin/linux/ubuntu trusty/'
 sudo apt-get update
 sudo apt-get install r-base
+sudo mkdir -p /opt/rlib
+sudo chown risikiyim.risikiyim /opt/rlib
+export R_LIBS_USER=/opt/rlib
+export R_LIBS_SITE=/opt/rlib
+
 
