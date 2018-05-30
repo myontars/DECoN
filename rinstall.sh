44172371b9da0ba25b4f0749f95b4183c2bfbc80
Mustafa <myontars@users.noreply.github.com>
2018-05-30 13:00:06 +0300

    Update rinstall.sh

--- rinstall.sh
+++ rinstall.sh
@@ -1,7 +1,8 @@
+#R_LIBS_USER=/opt/rlib/ Rscript makeCNVcalls.R
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
 sudo add-apt-repository 'deb [arch=amd64,i386] https://cran.rstudio.com/bin/linux/ubuntu trusty/'
 sudo apt-get update
-sudo apt-get install r-base
+sudo apt-get install r-base -y
 sudo mkdir -p /opt/rlib
 sudo chown risikiyim.risikiyim /opt/rlib
 export R_LIBS_USER=/opt/rlib/
